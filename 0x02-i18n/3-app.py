78c1ff62ce0746065810ffec34169b9630abc65c
shalom ogoziem <shalomzymike@gmail.com>
2024-02-06 16:52:44 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -18,19 +18,19 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale() -> Any:
-    """determines the locale/language of the application """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+app.config.from_object("3-app.Config")
 
 
 @app.route('/', strict_slashes=False)
 def hello_world() -> Any:
     """ hello world output"""
     return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale() -> Any:
+    """determines the locale/language of the application """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 if __name__ == "__main__":
